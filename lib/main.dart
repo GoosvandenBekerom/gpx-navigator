9fadc9c242fd2f73d24b3701a2ff797a35cd7969
gvandenbekerom <gvandenbekerom@bol.com>
2023-10-03 22:33:15 +0200

    feat: setup geolocation stream and permissions

--- lib/main.dart
+++ lib/main.dart
@@ -2,37 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:gpx_navigator/streetmap.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const GpxNavigator());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GpxNavigator extends StatelessWidget {
+  const GpxNavigator({super.key});
 
-  // This widget is the root of your application.
+  // Root Widget
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GPX Navigator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigoAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'GPX Navigator'),
     );
   }
 }
--- lib/streetmap.dart
+++ lib/streetmap.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_map/flutter_map.dart';
+import 'package:gpx_navigator/location.dart';
 import 'package:latlong2/latlong.dart';
 
-class StreetMap extends StatelessWidget {
+class StreetMap extends StatefulWidget {
   const StreetMap({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _StreetMapState();
+}
+
+class _StreetMapState extends State<StreetMap> {
+  LatLng? location;
+
+  @override
+  initState() {
+    super.initState();
+
+    getLocationStream()
+        .then((stream) => {
+              stream.listen((currentLocation) {
+                setState(() {
+                  location = currentLocation;
+                  print('lat: ${currentLocation.latitude}, long: ${currentLocation.longitude}');
+                });
+              })
+            })
+        .catchError((error, stackTrace) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(error.toString())));
+        }); // todo: handle error properly
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
       options: MapOptions(
-        center: const LatLng(51.509364, -0.128928),
+        center: location,
         zoom: 9.2,
       ),
       children: [
